0c0fc8e4b9d5bd04dec35a8542e6a0a475cfbfab
cldhfleks2 <cldhfleks2@gmail.com>
2024-07-02 19:22:53 +0900

    240702 관리자페이지 완료

--- staticfiles/js/manager/teacher/teacher.js
+++ staticfiles/js/manager/teacher/teacher.js
@@ -1,5 +1,4 @@
 // 삭제 버튼 누르면 뜨는 모달창
-// 삭제 버튼 누르면 뜨는 모달창
 document.addEventListener("DOMContentLoaded", function () {
   const deleteButtons = document.querySelectorAll(".delete-button");
   const modalWrap = document.querySelector(".delete-modal-wrap");
@@ -24,70 +23,39 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-//아래 게시물 창 버튼
-const paginationBtn = document.querySelectorAll(".page-count-num");
-const paginationBox = document.querySelector(".page");
-
-paginationBox.addEventListener("click", (e) => {
-  let pageBtn = e.target.closest("button.page-count-num");
-  if (pageBtn) {
-    paginationBtn.forEach((item) => {
-      item.classList.contains("page-count-num") &&
-        item.classList.remove("page-count-num-choice");
-    });
-    pageBtn.classList.add("page-count-num-choice");
-  }
-});
 
 // 글쓰기 버튼을 클릭하면 모달이 생성되고 다시 클릭하면 모달이 없어져야함
 // 대신 화면의 다른 부분을 클릭해도 모달이 없어져야함
-const modalButton = document.querySelector("button.list-order");
-const modal = document.querySelector(".list-order-function");
-const modalUl = document.querySelector("ul.list-order-function");
-const modalSvg = document.querySelector("svg.list-order");
+// const modalButton = document.querySelector("button.list-order");
+// const modal = document.querySelector(".list-order-function");
+// const modalUl = document.querySelector("ul.list-order-function");
+// const modalSvg = document.querySelector("svg.list-order");
 
-document.addEventListener("click", (e) => {
-  if (e.target.closest("button.list-order")) {
-    modal.style.display = "block";
-    modalSvg.style.transform = "rotate(180deg)";
-    modalButton.classList.add("border-color");
-  } else {
-    if (e.target.classList.contains("list-order-function")) {
-      modal.style.display = "block";
-      return;
-    }
-    modal.style.display = "none";
-    modalSvg.style.transform = "rotate(360deg)";
-    modalButton.classList.remove("border-color");
-  }
-});
+// document.addEventListener("click", (e) => {
+//   if (e.target.closest("button.list-order")) {
+//     modal.style.display = "block";
+//     modalSvg.style.transform = "rotate(180deg)";
+//     modalButton.classList.add("border-color");
+//   } else {
+//     if (e.target.classList.contains("list-order-function")) {
+//       modal.style.display = "block";
+//       return;
+//     }
+//     modal.style.display = "none";
+//     modalSvg.style.transform = "rotate(360deg)";
+//     modalButton.classList.remove("border-color");
+//   }
+// });
 
 // 순서 정렬 박스에서 선택한 값이 위에 선택하기
-const modalBtns = modal.querySelectorAll("button.function-latest");
-modalBtns.forEach((modalBtn) => {
-  modalBtn.addEventListener("click", (e) => {
-    const btn = e.target.closest("button");
-    const order1 = document.querySelector(".order-1");
-    order1.innerText = btn.innerText;
-  });
-});
-
-// 강의 정보, 리뷰 정보, 수강생 목록 선택하기
-const catebtns = document.querySelectorAll("#btn");
-const cateUnder = document.querySelectorAll("#under");
-cateUnder[0].classList.add("underbar-checked");
-catebtns[0].classList.add("lecture-checked");
-
-catebtns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    catebtns.forEach((btn, i) => {
-      btn.classList.remove("lecture-checked");
-      cateUnder[i].classList.remove("underbar-checked");
-    });
-    btn.classList.add("lecture-checked");
-    cateUnder[i].classList.add("underbar-checked");
-  });
-});
+// const modalBtns = modal.querySelectorAll("button.function-latest");
+// modalBtns.forEach((modalBtn) => {
+//   modalBtn.addEventListener("click", (e) => {
+//     const btn = e.target.closest("button");
+//     const order1 = document.querySelector(".order-1");
+//     order1.innerText = btn.innerText;
+//   });
+// });
 
 // 검색창 눌렀을때 검색바에 아웃라인주기
 const searchBar = document.querySelector("label.search-bar");
@@ -168,3 +136,49 @@ checkboxes.forEach(function (checkbox) {
     allCheck.checked = allChecked;
   });
 });
+
+
+
+
+
+// 강사등록버튼
+const addBtn = document.querySelector(".addBtn");
+const addModal = document.querySelector(".musclematrix-layer");
+const closeBtn = document.querySelector(".btn-close")
+
+addBtn.addEventListener('click', function() {
+  addModal.style.display = "block"
+});
+
+closeBtn.addEventListener("click", function(){
+  addModal.style.display = "none"
+})
+
+// 강사 이미지 첨부란
+document.addEventListener('DOMContentLoaded', function() {
+  const teacherPic = document.querySelector('.teacher-pic');
+  const teacherImageInput = document.getElementById('teacherImageInput');
+  const teacherImage = document.getElementById('teacherImage');
+
+  teacherPic.addEventListener('click', function() {
+      teacherImageInput.click();
+  });
+
+  teacherImageInput.addEventListener('change', function(event) {
+      const file = event.target.files[0];
+      if (file) {
+          const reader = new FileReader();
+          reader.onload = function(e) {
+              teacherImage.src = e.target.result;
+          }
+          reader.readAsDataURL(file);
+      }
+  });
+});
+
+
+
+
+
+
+
